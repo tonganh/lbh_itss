f8894587958c92163a14c6679079f890029d0615
Thành Vũ <thanhvu@Thanhs-MacBook-Pro.local>
2022-12-18 02:42:08 -0800

    postgres

--- src/models/index.js
+++ src/models/index.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
   process.env.DATABASE_USER,
   process.env.DATABASE_PASSWORD,
   {
-    dialect: "mysql",
+    dialect: "postgres",
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
   }
