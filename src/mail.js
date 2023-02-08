4c3e66c0fbb735595eba9e8967204ec325ab1681
anhtn <tongngocanhcampha@gmail.com>
2023-02-08 22:17:35 +0700

    Done for LBH ML

--- src/mail.js
+++ src/mail.js
@@ -2,11 +2,12 @@ const CronJob = require("cron").CronJob;
 const nodemailer = require("nodemailer");
 const { models } = require("./models");
 const { Op } = require("sequelize");
-
+const axios = require('axios').default
 const configOptions = {
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
+  service: 'gmail',
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
@@ -52,10 +53,66 @@ module.exports.createDailyJob = () => {
     "Asia/Ho_Chi_Minh"
   );
   job.start();
+
+  const job2 = new CronJob(
+    "* * * * *",
+    async () => {
+      const { Task, User } = models
+      const listAllTask = await Task.findAll()
+      await Promise.all(listAllTask.map(async (task) => {
+        const { user_id, start_time, process, title, description, content } = task
+        if (process === 100) {
+          return
+        }
+        const presentTime = new Date().setSeconds(0, 0)
+        const dateAfterFormatSecond = new Date(start_time).setSeconds(0, 0)
+        const startTimeFormated = new Date(dateAfterFormatSecond)
+
+        const diffMs = presentTime - dateAfterFormatSecond
+        const diffMins = Math.round((diffMs) / 60000); // minutes
+        console.log("🚀 ~ file: mail.js:67 ~ awaitPromise.all ~ diffMins", diffMins)
+        const diffMinuteNeedSendMail = 16
+        // const conditionSendMail = diffMins === diffMinuteNeedSendMail
+        const conditionSendMail = diffMins === 15 || diffMins === 0
+        if (conditionSendMail) {
+          const userNeedSendMail = await User.findOne({
+            where: {
+              id: user_id
+            }
+          })
+          const { email, name } = userNeedSendMail
+          const remindTimeContent = diffMins < 0 ? 'Đã bị quá hạn!' : 'Chỉ còn 15 phút nữa là đến deadline rồi, nhanh cái tay lên bạn ơi!!!'
+          const contentSendMail = `
+            ${name} thân mến\
+            ${remindTimeContent} \
+            Dưới đây là tóm tắt công việc lúc 16h hôm nay của bạn:\
+            - Nhiệm vụ của bạn là: ${title}\
+            - Mô tả: ${description}\
+            - Nội dung: ${content}\
+            - Thời gian: ${startTimeFormated.getHours() + 1} giờ ${startTimeFormated.getMinutes()} phút\
+          `;
+          try {
+            const linkEmailUseForsend = 'https://itss-final-project.2soft.top/auth/support-lbh'
+            const sendMailRes = await axios.post(linkEmailUseForsend, {
+              email: email, content: contentSendMail
+            })
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      }))
+      console.log("Running Send Mail Job");
+    },
+    null,
+    true,
+    "Asia/Ho_Chi_Minh"
+  );
+  job2.start();
 };
 
 module.exports.sendMail = (email, subject, content) => {
   const transporter = nodemailer.createTransport(configOptions);
+  console.log("🚀 ~ file: mail.js:98 ~ configOptions", configOptions)
   const mailOptions = {
     from: "Sunflower ITSS",
     to: email,
